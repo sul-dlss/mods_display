de1f5f039b434788bb34645b488ae4de9a9ffded
Jessie Keck <jessie.keck@gmail.com>
2013-05-15 18:09:52 -0700

    Add some logic to the Imprint class to get better lables and parse non-imprint origin_info fields.

--- lib/mods_display/fields/imprint.rb
+++ lib/mods_display/fields/imprint.rb
@@ -2,34 +2,81 @@ class ModsDisplay::Imprint < ModsDisplay::Field
 
   def label
     return super unless super.nil?
-    "Imprint"
+    if imprint?
+      "Imprint"
+    else
+      origin_info_labels[origin_info_part]
+    end
+  end
+
+  def values
+    imprint_data = []
+    origin_info_data = []
+    @value.children.each do |child|
+      if imprint_parts.include?(child.name.to_sym)
+        imprint_data << child
+      elsif origin_info_parts.include?(child.name.to_sym)
+        origin_info_data << child
+      end
+    end
+    {:imprint => imprint_data, :origin_info => origin_info_data}
   end
 
   def text
     return super unless super.nil?
     output = []
-    imprint_parts.each do |part|
-      if @value.respond_to? part
-        imprint_part = @value.send(part)
-        attributes = imprint_part.attributes.first || {}
-        unless ( ["dateCreated", "dateIssued"].include?(imprint_part.name.join) and
-                 attributes.has_key?("encoding") )
-          output << imprint_part.text
+    if imprint?
+      imprint_parts.each do |part|
+        if @value.respond_to? part
+          imprint_part = @value.send(part)
+          attributes = imprint_part.attributes.first || {}
+          unless ( ["dateCreated", "dateIssued"].include?(imprint_part.name.join) and
+                   attributes.has_key?("encoding") )
+            output << imprint_part.text
+          end
         end
       end
+    else
+      output << @value.text.strip
     end
     output.join(" ").strip
   end
 
   def to_html
-    return nil if text.strip == ""
+    return nil if text.strip == "" or (values[:imprint] == [] && values[:origi_info] == [])
     super
   end
 
   private
 
+  def imprint?
+    imprint_parts.any?{ |part| !@value.send(part).nil? and @value.send(part).length > 0 }
+  end
+
+  def origin_info_part
+    @value.children.map do |child|
+      unless child.name == "text"
+        child.name
+      end
+    end.compact.first.to_sym
+  end
+
   def imprint_parts
     [:place, :publisher, :dateCreated, :dateIssued, :dateCaptured, :dateOther]
+  end
+
+  def origin_info_parts
+    origin_info_labels.keys
+  end
+
+  def origin_info_labels
+    {:dateValid     => "Date Valid",
+     :dateModified  => "Date Modified",
+     :copyrightDate => "Copyright Date",
+     :edition       => "Edition",
+     :issuance      => "Issuance",
+     :frequency     => "Frequency"
+    }
   end
 
 end
--- spec/fields/imprint_spec.rb
+++ spec/fields/imprint_spec.rb
@@ -23,12 +23,63 @@ describe ModsDisplay::Imprint do
         </originInfo>
       </mods>
     MODS
+    origin_info_mods = <<-MODS
+      <mods>
+        <originInfo>
+          <dateValid>A Valid Date</dateValid>
+        </originInfo>
+        <originInfo>
+          <edition>The Edition</edition>
+        </originInfo>
+      </mods>
+    MODS
+    encoded_date = <<-MODS
+      <mods>
+        <originInfo>
+          <place>A Place</place>
+          <dateIssued encoding="an-encoding">An Encoded Date</dateIssued>
+        </originInfo>
+      </mods>
+    MODS
+    mixed_mods = <<-MODS
+      <mods>
+        <originInfo>
+          <place>A Place</place>
+          <publisher>A Publisher</publisher>
+          <edition>The Edition</edition>
+        </originInfo>
+      </mods>
+    MODS
     @imprint = Stanford::Mods::Record.new.from_str(imprint_mods, false).origin_info.first
+    @date_valid = Stanford::Mods::Record.new.from_str(origin_info_mods, false).origin_info.first
+    @edition = Stanford::Mods::Record.new.from_str(origin_info_mods, false).origin_info.last
+    @encoded_date = Stanford::Mods::Record.new.from_str(encoded_date, false).origin_info.first
+    @mixed = Stanford::Mods::Record.new.from_str(mixed_mods, false).origin_info.first
   end
-  
+
+  describe "labels" do
+    it "should get the Imprint label by default" do
+      mods_display_imprint(@imprint).label.should == "Imprint"
+    end
+    it "should get the label from non-imprint origin info fields" do
+      mods_display_imprint(@date_valid).label.should == "Date Valid"
+      mods_display_imprint(@edition).label.should == "Edition"
+    end
+  end
+
   describe "values" do
     it "should return various parts of the imprint" do
       mods_display_imprint(@imprint).text.should == "A Place A Publisher A Create Date An Issue Date A Capture Date Another Date"
+    end
+    it "should get the text for non-imprint origin info fields" do
+      mods_display_imprint(@date_valid).text.should == "A Valid Date"
+      mods_display_imprint(@edition).text.should == "The Edition"
+    end
+    it "should omit dates with an encoding attribute" do
+      mods_display_imprint(@encoded_date).text.should_not include("An Encoded Date")
+    end
+    it "should handle mixed mods properly" do
+      pending
     end
   end
 end
