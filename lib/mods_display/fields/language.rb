2324a3dafac743f8484f7c4e58871deee31306c3
Jessie Keck <jessie.keck@gmail.com>
2013-05-21 11:20:12 -0700

    Only get language codes (not text) for Language class.

--- lib/mods_display/fields/language.rb
+++ lib/mods_display/fields/language.rb
@@ -5,7 +5,15 @@ class ModsDisplay::Language < ModsDisplay::Field
   end
 
   def fields
-    [ModsDisplay::Values.new(:label => label || "Language", :values => [text])]
+    if @value.languageTerm.length > 0
+      return_values = []
+      @value.languageTerm.select do |term|
+        term.attributes["type"].respond_to?(:value) && term.attributes["type"].value == "code"
+      end.each do |term|
+        return_values << language_codes[term.text]
+      end
+      [ModsDisplay::Values.new(:label => label || "Language", :values => return_values)] unless return_values.empty?
+    end
   end
 
   def text
--- spec/fields/language_spec.rb
+++ spec/fields/language_spec.rb
@@ -6,9 +6,11 @@ end
 
 describe ModsDisplay::Language do
   before(:all) do
-    @language = Stanford::Mods::Record.new.from_str("<mods><language><languageTerm>eng</languageTerm></language></mods>", false).language.first
-    @display_label = Stanford::Mods::Record.new.from_str("<mods><language displayLabel='Lang'>eng</language></mods>", false).language.first
-    @no_lang = Stanford::Mods::Record.new.from_str("<mods><language displayLabel='Lang'>zzzxxx</language></mods>", false).language.first
+    @language = Stanford::Mods::Record.new.from_str("<mods><language><languageTerm type='code'>eng</languageTerm></language></mods>", false).language.first
+    @display_label = Stanford::Mods::Record.new.from_str("<mods><language displayLabel='Lang'><languageTerm type='code'>eng</languageTerm></language></mods>", false).language.first
+    @no_lang = Stanford::Mods::Record.new.from_str("<mods><language displayLabel='Lang'><languageTerm type='code'>zzzxxx</languageTerm></language></mods>", false).language.first
+    @mixed = Stanford::Mods::Record.new.from_str("<mods><language><languageTerm type='text'>ger</languageTerm><languageTerm type='code'>eng</languageTerm></language></mods>", false).language.first
+    @multi = Stanford::Mods::Record.new.from_str("<mods><language><languageTerm type='code'>ger</languageTerm><languageTerm type='code'>eng</languageTerm></language></mods>", false).language.first
     @display_form = Stanford::Mods::Record.new.from_str("<mods><language><languageTerm>zzzxxx</languageTerm><displayForm>Klingon</displayForm></language></mods>", false).language.first
   end
   describe "label" do
@@ -26,6 +28,16 @@ describe ModsDisplay::Language do
       values.first.should be_a ModsDisplay::Values
       values.first.label.should == "Lang"
       values.first.values.should == ["English"]
+    end
+    it "should not return any non type='code' languageTerms from the XML" do
+      values = mods_display_language(@mixed).fields
+      values.length.should == 1
+      values.first.values.should == ["English"]
+    end
+    it "should handle multiple languages correctly" do
+      values = mods_display_language(@multi).fields
+      values.length.should == 1
+      values.first.values.should == ["German", "English"]
     end
   end
   describe "text" do
