008292576d46d982179999cbf533f58513c0e22d
Jessie Keck <jessie.keck@gmail.com>
2013-05-24 23:07:10 -0700

    Collapse matching and adjascent labels in notes.

--- lib/mods_display/fields/note.rb
+++ lib/mods_display/fields/note.rb
@@ -2,8 +2,28 @@ class ModsDisplay::Note < ModsDisplay::Field
   
   def fields
     return_values = []
-    @value.each do |val|
-      return_values << ModsDisplay::Values.new(:label => displayLabel(val) || note_label(val), :values => [val.text])
+    current_label = nil
+    prev_label = nil
+    buffer = []
+    @value.each_with_index do |val, index|
+      current_label = (displayLabel(val) || note_label(val))
+      if @value.length == 1
+        return_values << ModsDisplay::Values.new(:label => current_label, :values => [val.text])
+      elsif index == (@value.length-1)
+        # need to deal w/ when we have a last element but we have separate labels in the buffer.
+        if current_label != prev_label
+          return_values << ModsDisplay::Values.new(:label => prev_label, :values => buffer.flatten)
+          return_values << ModsDisplay::Values.new(:label => current_label, :values => [val.text])
+        else
+          buffer << people
+          return_values << ModsDisplay::Values.new(:label => current_label, :values => buffer.flatten)
+        end
+      elsif prev_label and (current_label != prev_label)
+        return_values << ModsDisplay::Values.new(:label => prev_label, :values => buffer.flatten)
+        buffer = []
+      end
+      buffer << val.text
+      prev_label = current_label
     end
     return_values
   end
--- spec/fields/note_spec.rb
+++ spec/fields/note_spec.rb
@@ -10,6 +10,7 @@ describe ModsDisplay::Note do
     @display_label = Stanford::Mods::Record.new.from_str("<mods><note displayLabel='Special Label'>Note Field</note></mods>", false).note
     @sor_label = Stanford::Mods::Record.new.from_str("<mods><note type='statement of responsibility'>Note Field</note></mods>", false).note
     @type_label = Stanford::Mods::Record.new.from_str("<mods><note type='Some other Type'>Note Field</note></mods>", false).note
+    @complex_label = Stanford::Mods::Record.new.from_str("<mods><note>Note Field</note><note>2nd Note Field</note><note type='statement of responsibility'>SoR</note><note>Another Note</note></mods>", false).note
   end
   describe "label" do
     it "should have a default label" do
@@ -24,6 +25,29 @@ describe ModsDisplay::Note do
     it "should use use the raw type attribute if one is present" do
       mods_display_note(@type_label).fields.first.label.should == "Some other Type"
     end
-  end  
+  end
+  
+  describe "fields" do
+    it "should handle single values" do
+      fields = mods_display_note(@note).fields
+      fields.length.should == 1
+      fields.first.values.should == ["Note Field"]
+    end
+    it "should handle complex grouping" do
+      fields = mods_display_note(@complex_label).fields
+      fields.length.should == 3
+      fields.first.label.should == "Note"
+      fields.first.values.length == 2
+      fields.first.values.should == ["Note Field", "2nd Note Field"]
+      
+      fields[1].label == "Statement of Responsibility"
+      fields[1].values.length == 1
+      fields[1].values.should == ["SoR"]
+      
+      fields.last.label.should == "Note"
+      fields.last.values.length == 1
+      fields.last.values.should == ["Another Note"]
+    end
+  end
   
 end
