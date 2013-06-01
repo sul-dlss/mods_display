b45626f454e57ab9b8e90144bba187362db4cf67
Jessie Keck <jessie.keck@gmail.com>
2013-05-31 17:38:23 -0700

    Fall back on default fields and use displayLabel for labeling.

--- lib/mods_display/fields/location.rb
+++ lib/mods_display/fields/location.rb
@@ -1,23 +1,17 @@
 class ModsDisplay::Location < ModsDisplay::Field
-  
-  def fields
-    return_values = []
-    @value.each do |val|
-      return_values << ModsDisplay::Values.new(:label => label || location_label(val), :values => [val.text])
-    end
-    return_values
-  end
-  
+
   private
-  
+
+  def displayLabel(element)
+    super(element) || location_label(element) || "Location"
+  end
+
   def location_label(element)
     if element.attributes["type"].respond_to?(:value) && location_labels.has_key?(element.attributes["type"].value)
       location_labels[element.attributes["type"].value]
-    else
-      "Location"
     end
   end
-  
+
   def location_labels
     {"repository" => "Repository"}
   end
