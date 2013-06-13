bb251be7d401e7466dadd8059ee1df7874698609
Jessie Keck <jessie.keck@gmail.com>
2013-06-13 11:31:01 -0700

    Collapse adjacent/like labels in the PhysicalDescription XML.

--- lib/mods_display/fields/description.rb
+++ lib/mods_display/fields/description.rb
@@ -1,14 +1,35 @@
 class ModsDisplay::Description < ModsDisplay::Field
 
   def fields
-    description_fields.map do |description|
-      ModsDisplay::Values.new({:label => (label || description_label(description) || labels[description.name.to_sym] || "Physical Description"),
-                               :values => [description.text]})
+    return_values = []
+    current_label = nil
+    prev_label = nil
+    buffer = []
+    description_fields.each_with_index do |val, index|
+      current_label = description_label(val)
+      if description_fields.length == 1
+        return_values << ModsDisplay::Values.new(:label => current_label, :values => [val.text])
+      elsif index == (description_fields.length-1)
+        # need to deal w/ when we have a last element but we have separate labels in the buffer.
+        if current_label != prev_label
+          return_values << ModsDisplay::Values.new(:label => prev_label, :values => buffer.flatten)
+          return_values << ModsDisplay::Values.new(:label => current_label, :values => [val.text])
+        else
+          buffer << val.text
+          return_values << ModsDisplay::Values.new(:label => current_label, :values => buffer.flatten)
+        end
+      elsif prev_label and (current_label != prev_label)
+        return_values << ModsDisplay::Values.new(:label => prev_label, :values => buffer.flatten)
+        buffer = []
+      end
+      buffer << val.text
+      prev_label = current_label
     end
+    return_values
   end
 
   def description_label(element)
-    element.attributes["displayLabel"].value if element.attributes["displayLabel"].respond_to?(:value)
+    label || displayLabel(element) || labels[element.name.to_sym] || "Physical Description"
   end
 
   private
