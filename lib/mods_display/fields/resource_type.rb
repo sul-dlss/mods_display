3d4f93dbf4cb62bc6d22a577abaa34790786a2ef
Jessie Keck <jessie.keck@gmail.com>
2013-09-05 17:47:25 -0700

    METADOR-18: Capitalize typeOfResource values.

--- lib/mods_display/fields/resource_type.rb
+++ lib/mods_display/fields/resource_type.rb
@@ -1,5 +1,12 @@
 class ModsDisplay::ResourceType < ModsDisplay::Field
 
+  def fields
+    return_fields = @values.map do |value|
+      ModsDisplay::Values.new(:label => displayLabel(value) || label, :values => [value.text.strip.capitalize].flatten)
+    end
+    collapse_fields(return_fields)
+  end
+
   private
   def displayLabel(element)
     super(element) || "Type of resource"
--- spec/fields/resource_type_spec.rb
+++ spec/fields/resource_type_spec.rb
@@ -7,6 +7,7 @@ end
 describe ModsDisplay::ResourceType do
   before(:all) do
     @type = Stanford::Mods::Record.new.from_str("<mods><typeOfResource>Resource Type</typeOfResource></mods>", false).typeOfResource
+    @downcase = Stanford::Mods::Record.new.from_str("<mods><typeOfResource>resource type</typeOfResource></mods>", false).typeOfResource
     @display_label = Stanford::Mods::Record.new.from_str("<mods><typeOfResource displayLabel='Special label'>Resource Type</typeOfResource></mods>", false).typeOfResource
   end
   it "should default to a label of 'Type of resource'" do
@@ -18,5 +19,10 @@ describe ModsDisplay::ResourceType do
     fields = mods_display_resource_type(@display_label).fields
     fields.length.should == 1
     fields.first.label.should == "Special label"
+  end
+  it "should capitalize the first letter of the values" do
+    fields = mods_display_resource_type(@downcase).fields
+    fields.length.should == 1
+    fields.first.values.should == ["Resource type"]
   end
 end
