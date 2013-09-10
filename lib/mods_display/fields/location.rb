da7fd3e72550ce63ef440858731f7ce22ab54691
Jessie Keck <jessie.keck@gmail.com>
2013-09-10 16:24:35 -0700

    METADOR-26: Add support for child element displayLabels and link URLs to their displayLabels (if available).

--- lib/mods_display/fields/location.rb
+++ lib/mods_display/fields/location.rb
@@ -1,13 +1,35 @@
 class ModsDisplay::Location < ModsDisplay::Field
 
+  def fields
+    return_fields = []
+    @values.each do |location|
+      location.children.each do |child|
+        if location_field_keys.include? child.name.to_sym
+          if child.name.to_sym == :url
+            loc_label = displayLabel(location) || "Location"
+            value = "<a href='#{child.text}'>#{displayLabel(child) || child.text}</a>"
+          else
+            loc_label = location_label(child) || displayLabel(location) || "Location"
+            value = child.text
+          end
+          return_fields << ModsDisplay::Values.new(:label  => loc_label || displayLabel(location) || "Location",
+                                                   :values => [value])
+        end
+      end
+    end
+    collapse_fields(return_fields)
+  end
+
   private
 
-  def displayLabel(element)
-    super(element) || location_label(element) || "Location"
+  def location_field_keys
+    [:physicalLocation, :url, :shelfLocation, :holdingSimple, :holdingExternal]
   end
 
   def location_label(element)
-    if element.attributes["type"].respond_to?(:value) && location_labels.has_key?(element.attributes["type"].value)
+    if displayLabel(element)
+      displayLabel(element)
+    elsif element.attributes["type"].respond_to?(:value) && location_labels.has_key?(element.attributes["type"].value)
       location_labels[element.attributes["type"].value]
     end
   end
--- spec/fields/location_spec.rb
+++ spec/fields/location_spec.rb
@@ -7,8 +7,9 @@ end
 describe ModsDisplay::Note do
   before(:all) do
     @location = Stanford::Mods::Record.new.from_str("<mods><location><physicalLocation>The Location</physicalLocation></location></mods>", false).location
+    @urls = Stanford::Mods::Record.new.from_str("<mods><location><url displayLabel='Stanford University Library'>http://library.stanford.edu</url></location><location displayLabel='PURL'><url>http://purl.stanford.edu</url></location></mods>", false).location
     @display_label = Stanford::Mods::Record.new.from_str("<mods><location displayLabel='Special Label'><shelfLocation>On Shelf A</shelfLocation></location></mods>", false).location
-    @repository_label = Stanford::Mods::Record.new.from_str("<mods><location type='repository'><physicalLocation>Location Field</physicalLocation></location></mods>", false).location
+    @repository_label = Stanford::Mods::Record.new.from_str("<mods><location><physicalLocation type='repository'>Location Field</physicalLocation></location></mods>", false).location
   end
   describe "label" do
     it "should have a default label" do
@@ -17,9 +18,27 @@ describe ModsDisplay::Note do
     it "should use the displayLabel attribute when one is available" do
       mods_display_location(@display_label).fields.first.label.should == "Special Label"
     end
+    it "should handle the URL labels correctly" do
+      mods_display_location(@urls).fields.map{|f| f.label}.should == ["Location", "PURL"]
+    end
     it "should use get a label from a list of translations" do
       mods_display_location(@repository_label).fields.first.label.should == "Repository"
     end
   end  
-  
+  describe "fields" do
+    describe "URLs" do
+      it "should link and use the displayLabel as text" do
+        fields = mods_display_location(@urls).fields
+        fields.length.should == 2
+        field = fields.find{|f| f.label == "Location"}
+        field.values.should == ["<a href='http://library.stanford.edu'>Stanford University Library</a>"]
+      end
+      it "should link the URL itself in the absence of a displayLabel on the url element" do
+        fields = mods_display_location(@urls).fields
+        fields.length.should == 2
+        field = fields.find{|f| f.label == "PURL"}
+        field.values.should == ["<a href='http://purl.stanford.edu'>http://purl.stanford.edu</a>"]
+      end
+    end
+  end
 end
