36e689dce54a19715671ce6698017aefe815b586
Jessie Keck <jessie.keck@gmail.com>
2015-11-03 14:43:50 -0800

    Rubocop autofixes and a few little refactors to better conform to community styles.

--- lib/mods_display/fields/collection.rb
+++ lib/mods_display/fields/collection.rb
@@ -1,21 +1,36 @@
-class ModsDisplay::Collection < ModsDisplay::Field
-
-  def label
-    super || I18n.t('mods_display.collection')
-  end
-
-  def fields
-    return_fields = []
-    @values.each do |value|
-      if value.respond_to?(:titleInfo) and
-         value.respond_to?(:typeOfResource) and
-         value.typeOfResource.attributes.length > 0 and
-         value.typeOfResource.attributes.first.has_key?("collection") and
-         value.typeOfResource.attributes.first["collection"].value == "yes"
-        return_fields << ModsDisplay::Values.new(:label => label, :values => [value.titleInfo.text.strip])
-      end
+module ModsDisplay
+  ###
+  #  Collection class to parse collection data out of Mods relatedItem fields
+  ###
+  class Collection < Field
+    def label
+      super || I18n.t('mods_display.collection')
     end
-    collapse_fields(return_fields)
-  end
 
+    def fields
+      return_fields = []
+      @values.each do |value|
+        next unless related_item_is_a_collection?(value)
+        return_fields << ModsDisplay::Values.new(
+          label: label,
+          values: [value.titleInfo.text.strip]
+        )
+      end
+      collapse_fields(return_fields)
+    end
+
+    private
+
+    def related_item_is_a_collection?(value)
+      value.respond_to?(:titleInfo) && resource_type_is_collection?(value)
+    end
+
+    def resource_type_is_collection?(value)
+      return unless value.respond_to?(:typeOfResource)
+      return unless value.typeOfResource.attributes.length > 0
+      value.typeOfResource.attributes.length > 0 &&
+        value.typeOfResource.attributes.first.key?('collection') &&
+        value.typeOfResource.attributes.first['collection'].value == 'yes'
+    end
+  end
 end
--- spec/fields/collection_spec.rb
+++ spec/fields/collection_spec.rb
@@ -1,31 +1,52 @@
-require "spec_helper"
+require 'spec_helper'
 
 def mods_display_collection(mods_record)
-  ModsDisplay::Collection.new(mods_record, ModsDisplay::Configuration::Base.new, double("controller"))
+  ModsDisplay::Collection.new(
+    mods_record,
+    ModsDisplay::Configuration::Base.new,
+    double('controller')
+  )
 end
 
 describe ModsDisplay::Collection do
-  before(:all) do
-    @collection = Stanford::Mods::Record.new.from_str("<mods><relatedItem><titleInfo><title>The Collection</title></titleInfo><typeOfResource collection='yes' /></relatedItem></mods>", false).related_item
-    @non_collection = Stanford::Mods::Record.new.from_str("<mods><relatedItem><titleInfo><title>Not a Collection</title></titleInfo></relatedItem></mods>", false).related_item
-    @display_label = Stanford::Mods::Record.new.from_str("<mods><relatedItem displayLabel='Special Collection'><titleInfo><title>Not a Collection</title></titleInfo></relatedItem></mods>", false).related_item
+  let(:collection) do
+    Stanford::Mods::Record.new.from_str(
+      '<mods><relatedItem><titleInfo><title>The Collection</title></titleInfo><typeOfResource collection="yes" /></relatedItem></mods>',
+      false
+    ).related_item
   end
-  describe "label" do
-    it "should default to Collection" do
-      expect(mods_display_collection(@collection).label).to eq("Collection:")
+  let(:non_collection) do
+    Stanford::Mods::Record.new.from_str(
+      '<mods><relatedItem><titleInfo><title>Not a Collection</title></titleInfo></relatedItem></mods>',
+      false
+    ).related_item
+  end
+  let(:display_label) do
+    Stanford::Mods::Record.new.from_str(
+      '<mods><relatedItem displayLabel="Special Collection"><titleInfo><title>Not a Collection</title></titleInfo></relatedItem></mods>',
+      false
+    ).related_item
+  end
+
+  describe 'label' do
+    it 'should default to Collection' do
+      expect(mods_display_collection(collection).label).to eq('Collection:')
     end
-    it "should get the displayLabel if available" do
-      expect(mods_display_collection(@display_label).label).to eq("Special Collection:")
+
+    it 'should get the displayLabel if available' do
+      expect(mods_display_collection(display_label).label).to eq('Special Collection:')
     end
   end
-  describe "fields" do
-    it "should get a collection title if there is an appropriate typeOfResource field with the collection attribute" do
-      fields = mods_display_collection(@collection).fields
+
+  describe 'fields' do
+    it 'should get a collection title if there is an appropriate typeOfResource field with the collection attribute' do
+      fields = mods_display_collection(collection).fields
       expect(fields.length).to eq(1)
-      expect(fields.first.values).to eq(["The Collection"])
+      expect(fields.first.values).to eq(['The Collection'])
     end
-    it "should not return anything if the there is not an appropriate typeOfResource field with the collection attribute" do
-      expect(mods_display_collection(@non_collection).fields).to eq([])
+
+    it 'should not return anything if the there is not an appropriate typeOfResource field with the collection attribute' do
+      expect(mods_display_collection(non_collection).fields).to eq([])
     end
   end
 end
