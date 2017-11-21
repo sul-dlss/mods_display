9d3d5c01a4a241f980173ad98a6c8f0a82b8f541
Darren Hardy <drh@stanford.edu>
2017-11-21 10:46:53 -0800

    For HTML rendering, converts Contents into <li> list using "--" as delimiter

--- lib/mods_display/fields/contents.rb
+++ lib/mods_display/fields/contents.rb
@@ -1,5 +1,23 @@
 module ModsDisplay
   class Contents < Field
+    def to_html
+      return nil if fields.empty? || @config.ignore?
+      output = ''
+      fields.each do |field|
+        next unless field.values.any? { |f| f && !f.empty? }
+        output << "<dt#{label_class} #{sanitized_field_title(field.label)}>#{field.label}</dt>"
+        output << "<dd#{value_class}>"
+        output << '<ul><li>'
+        # compress all values into a "--"-delimited string then split them up
+        output << field.values.join('--').split('--').map(&:strip).map do |val|
+          @config.link ? link_to_value(val.to_s) : link_urls_and_email(val.to_s)
+        end.join('</li><li>')
+        output << '</li></ul>'
+        output << '</dd>'
+      end
+      output
+    end
+
     private
 
     def displayLabel(element)
